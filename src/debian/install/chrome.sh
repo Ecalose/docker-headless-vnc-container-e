268ff38b23f55385dbe664eda413d19d9d5aa6be
Ecalose <47114714+Ecalose@users.noreply.github.com>
2023-08-02 07:40:20 +0800

    Update chrome.sh

--- src/debian/install/chrome.sh
+++ src/debian/install/chrome.sh
@@ -8,5 +8,5 @@ echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Deb
 curl -fsSL https://download.opensuse.org/repositories/home:ungoogled_chromium/Debian_Sid/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_ungoogled_chromium.gpg > /dev/null
 apt update
 apt install ungoogled-chromium -y
-ln -sfn /usr/bin/ungoogled-chromium /usr/bin/chromium-browser
+ln -sfn /usr/bin/chromium /usr/bin/chromium-browser
 apt-get clean -y
