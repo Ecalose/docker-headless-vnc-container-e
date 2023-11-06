5082fdf8085c189e92077c2d7e3c70462a1ea625
Ecalose <47114714+Ecalose@users.noreply.github.com>
2023-11-06 22:52:53 +0800

    Update tools.sh

--- src/debian/install/tools.sh
+++ src/debian/install/tools.sh
@@ -8,6 +8,6 @@ apt-get install -y vim wget net-tools locales bzip2 procps apt-utils \
     python3-numpy #used for websockify/novnc
 apt-get clean -y
 
-# echo "generate locales für en_US.UTF-8"
-# echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-# locale-gen
+echo "generate locales für en_US.UTF-8"
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
